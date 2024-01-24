80717de07f9d79ea563e53ccc8ad0f5913655b73
Rajib-ET <rajib@everestthemes.com>
2024-01-24 16:41:23 +0545

    Enqueue Dashicon in Frontend

--- omnipress-floating-button.php
+++ omnipress-floating-button.php
@@ -10,9 +10,11 @@ Author: Prabin Jha
 function omipressfb_enqueue_scripts() {
     wp_enqueue_style('omipressfb-style', plugin_dir_url(__FILE__) . 'assets/css/style.css');
     wp_enqueue_script('omipressfb-script', plugin_dir_url(__FILE__) . 'assets/js/script.js', array('jquery'), '1.0', true);
+    wp_enqueue_style( 'dashicons' );
 }
 
 add_action('wp_enqueue_scripts', 'omipressfb_enqueue_scripts');
+
 
 
 
